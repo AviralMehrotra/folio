3dedf9e74b866663ff196fa59def64807ef03fdc
Aviral Mehrotra <73425338+AviralMehrotra@users.noreply.github.com>
2026-01-11 17:57:43 +0530

    feat: completed the resume page

--- components/Stairs.jsx
+++ components/Stairs.jsx
@@ -27,7 +27,7 @@ const Stairs = () => {
             ease: "easeInOut",
             delay: reverseIndex(i) * 0.1,
           }}
-          className="h-full w-full bg-white relative"
+          className="h-full w-full bg-accent relative"
         />
       ))}
     </>
